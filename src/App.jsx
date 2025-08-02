2a13583486a7fdaef70fbdcea9b678d6e39b845a
7TIN <prasadgujarathi619@gmail.com>
2025-08-02 18:24:55 +0530

    feat: Login page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+// import { useState } from 'react';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  // const [email, setEmail] = useState('');
+  // const [password, setPassword] = useState('');
+
+
+  const handleOnSubmit = (e) =>{
+    e.preventDefault();
+
+    alert(`Successfully Logged in`);
+
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='card'>
+      <h3>
+        Login page
+      </h3>
+      <form className='form' onSubmit={handleOnSubmit}>
+        <div className='inputs'>
+          <label htmlFor="Email">Email</label>
+          <input type='text' placeholder='example@gmail.com' required
+          // onChange={(e) => setEmail(e.target.value)}
+          ></input>
+        </div>
+        <div className='inputs'>
+          <label htmlFor="Password">Password</label>
+          <input type='password' placeholder='password' required
+          // onChange={(e) => setPassword(e.target.value)}
+          ></input>
+        </div>
+        <div>
+          <button type='submit'>Login</button>
+        </div>
+      </form>
+    </div>
   )
 }
 
